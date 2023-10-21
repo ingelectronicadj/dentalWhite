37c7b774302885f25272d9ad6ff586f934a1d042
Diego Mena <diego.mena@unad.edu.co>
2023-10-21 12:20:56 -0500

    se carga icono para whatsapp flotante

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -1,6 +1,8 @@
 const scrollButton = document.getElementById('scroll-up');
+const whatsappButton = document.getElementById('whatsapp');
 function toggleScrollButton() {
     scrollButton.classList.toggle('showScrollUp', window.scrollY > 300);
+    whatsappButton.classList.toggle('showScrollUp', window.scrollY > 300);
 }
 
 window.addEventListener('scroll', toggleScrollButton);
